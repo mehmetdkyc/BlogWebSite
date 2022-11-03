ea53db17874bc3b35baed20bf413179880d79772
Mehmet Dokuyucu <50601213+mehmetdkyc@users.noreply.github.com>
2022-11-03 23:59:54 +0300

    Admin Panel eklendi.

---
+++ DotnetCoreKampı/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreKampı.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        CategoryManager cm;
+
+        public CategoryController(CategoryManager cm)
+        {
+            this.cm = cm;
+        }
+        public IActionResult Index()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
